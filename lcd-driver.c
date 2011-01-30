b4c207a748f40a42c4935168834b1bd699e6cfbe
pearsoal@gmail.com <pearsoal@gmail.com@630e69de-49cd-a4c4-3af7-bc87ec00d397>
2011-01-30 00:52:34 +0000

    Initial Import of main.c with functionality to initialize the LCD screen using the driver: lcd-driver.c.
    git-svn-id: http://handheld-i2c-analyzer.googlecode.com/svn/trunk@2 630e69de-49cd-a4c4-3af7-bc87ec00d397

---
+++ lcd-driver.c
@@ -0,0 +1,134 @@
+#include <avr\io.h>
+
+#define A0 		(1<<0)
+#define WR		(1<<1)
+#define RD		(1<<2)
+#define CS 		(1<<3)
+#define RESET 	(1<<4)
+
+#define CONTROL_PORT PORTE.OUT
+#define CONTROL_DIR PORTE.DIR
+#define CONTROL_PIN PORTE.IN
+
+#define DATA_PORT PORTD.OUT
+#define DATA_DIR PORTD.DIR
+#define DATA_PIN PORTD.IN
+
+void delay(unsigned int n)
+{
+  unsigned int i,j;
+  for (i=0; i<n; i++)
+	for (j=0; j<350; j++)
+	{;}
+}
+
+void data_out(unsigned char i)
+{
+  CONTROL_PORT &= ~(A0 | CS);
+  CONTROL_PORT &= ~(WR);
+  asm("nop");
+  DATA_PORT = i;
+  asm("nop");
+  asm("nop");
+  asm("nop");
+  asm("nop");
+  asm("nop");
+  CONTROL_PORT |= WR;
+  CONTROL_PORT |= CS;
+}
+
+void comm_out(unsigned char j)
+{
+  CONTROL_PORT |= A0;
+  CONTROL_PORT &= ~CS;
+  CONTROL_PORT &= ~(WR);
+  asm("nop");
+  DATA_PORT = j;
+  asm("nop");
+  asm("nop");
+  asm("nop");
+  asm("nop");
+  asm("nop");
+  CONTROL_PORT |= WR;
+  CONTROL_PORT |= CS;
+}
+
+void clear_display()
+{
+  int n;
+  comm_out(0x46);
+  data_out(0x00);
+  data_out(0x00);
+  comm_out(0x42);
+  for(n=0;n<1200;n++){
+	data_out(0x20);
+  }
+  comm_out(0x46);
+  data_out(0xB0);
+  data_out(0x04);
+  comm_out(0x42);
+  for(n=0;n<9600;n++){
+	data_out(0x00);
+  }
+}
+
+void reset_LCD()
+{
+	CONTROL_PORT &= ~RESET;
+	delay(5);
+	CONTROL_PORT |= RESET;
+	delay(10);
+}
+
+void init_LCD() 
+{
+  int i;
+  CONTROL_PORT |= RD;
+  delay(5);
+  comm_out(0x40); //Initialize device and display
+  delay(5);
+  data_out(0x30); 
+  data_out(0x87); 
+  data_out(0x07); 
+  data_out(0x27); 
+  data_out(0x2F); 
+  data_out(0xEF); 
+  data_out(0x28); 
+  data_out(0x00); 
+  comm_out(0x44); //SCROLL
+  data_out(0x00);
+  data_out(0x00);
+  data_out(0xF0);
+  data_out(0x80);
+  data_out(0x25);
+  data_out(0xF0);
+  data_out(0x00);
+  data_out(0x4B);
+  data_out(0x00);
+  data_out(0x00);
+  comm_out(0x5A); //HDOT SCR
+  data_out(0x00);
+  comm_out(0x5B); //OVLAY
+  data_out(0x01); 
+  comm_out(0x58); //Display off
+  data_out(0x56); 
+  comm_out(0x46); //Set cursor to first screen block
+  data_out(0x00);
+  data_out(0x00);
+  for(i = 0; i<9600; i++){
+    comm_out(0x42);
+    data_out(0xA0);
+  }
+  for(i = 0; i<9600 ; i++){
+    comm_out(0x42);
+    data_out(0x00);
+  }
+  comm_out(0x46);
+  data_out(0x00);
+  data_out(0x00);
+  comm_out(0x5D);
+  data_out(0x04);
+  data_out(0x86);
+  comm_out(0x59);
+  delay(5);
+}
---
+++ main.c
@@ -0,0 +1,34 @@
+/*
+  main.c
+  
+  This is the main function of the Handheld I2C Device
+*/
+#include <stdio.h>
+#include <stddef.h>
+#include <avr\io.h>
+#define F_CPU 32000000UL
+#include <util\delay.h>
+#include "lcd-driver.c"
+
+#define VERSION "0.0.1"
+
+int main(void)
+{
+  CCP = CCP_IOREG_gc; //Security Signature to modify clock
+  // initialize clock source to be 32MHz internal oscillator (no PLL)
+  OSC.CTRL = OSC_RC32MEN_bm; //enable internal 32MHz oscillator
+  while(!(OSC.STATUS & OSC_RC32MRDY_bm)); //wait for oscillator ready
+  CCP = CCP_IOREG_gc; //Security Signature to modify clock 
+  CLK.CTRL = CLK_SCLKSEL_RC32M_gc; //select sysclock 32MHz osc
+
+  CLK.PSCTRL = 0x00;
+  
+  CONTROL_DIR = 0xFF;
+  DATA_DIR = 0xFF;
+  asm("nop");
+  reset_LCD();
+  init_LCD();
+  
+  while(1){};
+  return 0;
+}
