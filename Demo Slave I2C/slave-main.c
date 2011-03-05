518777af2dbcb95c8326d8e75835261d79d11c2e
pearsoal@gmail.com <pearsoal@gmail.com@630e69de-49cd-a4c4-3af7-bc87ec00d397>
2011-03-05 03:32:12 +0000

    git-svn-id: http://handheld-i2c-analyzer.googlecode.com/svn/trunk@99 630e69de-49cd-a4c4-3af7-bc87ec00d397

--- Demo Slave I2C/slave-main.c
+++ Demo Slave I2C/slave-main.c
@@ -16,42 +16,14 @@
 #include <stdio.h>
 #include "lcd.h"
 
-volatile uint8_t new_flag = 0;
-volatile uint8_t data = 0;
-
 //				i2c_init
 //Initializes I2C bus to run at 100kHz and enables the bus
-/*void i2c_init(void){
-<<<<<<< .mine
+void i2c_init(void){
   DDRD |= (1<<PD0) | (1<<PD1);
   PORTD |= (1<<PD0) | (1<<PD1);
   //TWBR = 0x12;
-=======
-  //DDRD |= (1<<PD0) | (1<<PD1);
-  //PORTD |= (1<<PD0) | (1<<PD1);
-  //TWBR = 0x12;
->>>>>>> .r97
   TWAR = 0xFE;
   TWCR = (1<<TWEN) | (1<<TWEA);
-}*/
-void i2c_init(void){
-  DDRD |= (1<<PD0) | (1<<PD1);
-  //DDRD &= ~(1<<PD1);
-  TWAR = 0xFE;
-  TWCR = (1<<TWEN) | (1<<TWEA) | (1<<TWIE);
-}
-
-ISR(TWI_vect)
-{
-  //uint8_t status = TWSR & 0xF8;
-  if((TWSR & 0xF8) == 0x60){
-    TWCR = (1<<TWEA) | (1<<TWINT);
-  }
-  else if((TWSR & 0xF8) == 0x80){
-    new_flag = TRUE;
-    data = TWDR;
-    TWCR = (1<<TWEA) | (1<<TWINT);
-  }
 }
 
 //				spi_init
@@ -64,7 +36,7 @@ void spi_init(void){
   SPSR |= (1<<SPI2X);
 }
 
-/*int main(){
+int main(){
   spi_init();
   DDRF |= (1<<PF3);  //Set up LCD pin
   DDRD |= (1<<PD2) | (1<<PD3);
@@ -75,41 +47,13 @@ void spi_init(void){
     while(!(TWCR & (1<<TWINT))){}
     TWCR |= (1<<TWINT) | (1<<TWEA) | (1<<TWEN);
     while(!(TWCR & (1<<TWINT))){}
-<<<<<<< .mine
 	uint8_t data = TWDR;
 	TWCR |= (1<<TWINT) | (1<<TWEA) | (1<<TWEN);
-=======
-	TWCR = (1<<TWEN) | (1<<TWEA); //This may or may not do anything
-	uint8_t data = TWDR;
->>>>>>> .r97
     char string[20];
 	clear_display();
 	cursor_home();
     sprintf(string,"Recieved: %X",data);
     string2lcd(string);
-  }
-  return 0;
-}*/
-
-int main(){
-  spi_init();
-  DDRF |= (1<<PF3); //Set up LCD pin
-  DDRD |= (1<<PD2) | (1<<PD3);
-  PORTD &= ~((1<<PD2) | (1<<PD3));
-  lcd_init();
-  i2c_init();
-  sei();
-  while(1)
-  {
-    //if(new_flag == TRUE){
-	  new_flag = FALSE;
-	  char string[20];
-	  clear_display();
-	  cursor_home();
-	  sprintf(string, "Recieved: %X", TWDR);
-	  string2lcd(string);
-	//}
-	_delay_ms(100);
   }
   return 0;
 }
